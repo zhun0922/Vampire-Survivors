ac028699c2836caae37a01bc68c532b423b5959e
King ji hun <125746081+zhun0922@users.noreply.github.com>
2023-02-27 02:37:14 +0900

    애니메이션
    지금까지는main브랜치에 그대로 push했지만 이 이후 작업부턴 개발브랜치 및 pull request사용

--- TangTang/Assets/Script/Player.cs
+++ TangTang/Assets/Script/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     public Vector2 inputVec;
     Rigidbody2D rigid;
     SpriteRenderer spriter;
+    Animator anim;
     
     public float speed = 3;
 
@@ -16,6 +17,7 @@ public class Player : MonoBehaviour
         speed = 3;
         rigid = GetComponent<Rigidbody2D>();
         spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
   
     private void FixedUpdate()
@@ -33,6 +35,8 @@ public class Player : MonoBehaviour
 
     private void LateUpdate()
     {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
         if (inputVec.x != 0 )
         {
             //스프라이트 인스텍터창을 보면 flipX가 true , false값을 가지는 것을 알 수 있다
@@ -40,5 +44,8 @@ public class Player : MonoBehaviour
             //라고 쓰는게 맞지만 조건식을 그대로 넣어주자
             spriter.flipX = inputVec.x < 0; //우측 항의 조건식은 true이고, 이 값이 그대로 flipX에 적용된다
         }
+
+        Debug.Log(inputVec.magnitude);
+        
     }
 }
